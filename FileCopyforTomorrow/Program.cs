dad5de99287fed71e9197f4ba6b74364bdae9e87
大橋礼典 <ayanori1215@gmail.com>
2019-01-25 00:59:24 +0900

    [暫定版]今日の日付から明日の名前にリネームしてコピーを実施する

--- FileCopyforTomorrow/Program.cs
+++ FileCopyforTomorrow/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,56 @@ namespace FileCopyforTomorrow
     {
         static void Main(string[] args)
         {
+            string TargetFile;
+            string TomorrowFileName;
+            string TargetFileName;
+
+            TargetFile =  YesterdayFileName(DateTime.Now);
+            TomorrowFileName = AfterCopyName(DateTime.Now);
+
+            TargetFileName = @"C:\Data\TestFileReps\" + TargetFile;
+            
+            FileCopy_ChangeFileName(TomorrowFileName, TargetFileName);
         }
+
+        static string YesterdayFileName(DateTime dt)
+        {
+            string targetFile;
+
+            targetFile = dt.ToString("yyyyMMdd");
+            StringBuilder sb = new StringBuilder(targetFile);
+            sb.Append(".txt");
+            targetFile = sb.ToString();
+
+            return targetFile;
+        }
+
+
+
+        static string AfterCopyName(DateTime yesterday)
+        {
+            string todayFileName;
+            DateTime temp = yesterday.AddDays(1);
+
+            todayFileName = temp.ToString("yyyyMMdd");
+            StringBuilder sb = new StringBuilder(todayFileName);
+            sb.Append(".txt");
+            todayFileName = sb.ToString();
+
+            return todayFileName;
+        }
+
+        static void FileCopy_ChangeFileName(string todayFile, string yesterdayFile)
+        {
+            string todayFileName;
+            string Path = @"C:\Data\TestFileReps\";
+            todayFileName = Path + todayFile;
+            FileInfo f = new FileInfo(yesterdayFile);
+            f.CopyTo(todayFileName);
+        }
+
+
+
+
     }
 }
